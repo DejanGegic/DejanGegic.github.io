0d38de7a627ba30b7bd4b121547748367dd017d1
Dejan <dejangegic100@gmail.com>
2020-06-04 08:49:47 +0200

    ne radi, al da zapamtim

--- sicon/includes/visina.js
+++ sicon/includes/visina.js
@@ -1,6 +1,14 @@
+let kontejner = document.querySelectorAll(".containerChild");
+window.addEventListener("resize", provjera);
 provjera();
 
 function provjera() {
+    let visina = screen.availHeight;
+    for (let i = 0; i < kontejner.length; i++) {
+        kontejner[i].style.height = visina + "px";
+        console.log(kontejner[i].scrollHeight);
+    }
+
     var x = "Available Height: " + screen.availHeight;
     var y = "Height: " + screen.height;
     console.log(x);
